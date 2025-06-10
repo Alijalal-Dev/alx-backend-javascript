041972d42bf126a7b349b7c8b84c08ac82a4f101
Alijalal-Dev <aj0546006@gmail.com>
2025-06-10 21:12:43 +0100

    back to the old version

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -6,7 +6,7 @@ const app = http.createServer();
 
 
 app.on('request', (_, res) => {
-  const responseText = 'Hello ALX!';
+  const responseText = 'Hello Holberton School!';
 
   res.setHeader('Content-Type', 'text/plain');
   res.setHeader('Content-Length', responseText.length);
